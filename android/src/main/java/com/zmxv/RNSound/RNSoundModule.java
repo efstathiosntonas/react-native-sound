9c07b855670cbe75a446337d8648ca5018d307d0
James Reggio <james.reggio@gmail.com>
2017-09-29 15:39:34 -0400

    Fix premature collection of MediaPlayer on Android
    If the MediaPlayer takes a while to prepare, it's possible that a GC
    will cause the MediaPlayer to be collected between the time that
    `prepareAsync` is called and the time that the `OnPreparedListener` is
    invoked.
    
    By moving the assignment of the `playerPool` reference to immediately
    following the instantiation of the MediaPlayer, we eliminate the risk of
    premature collection.

--- android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
+++ android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
@@ -57,6 +57,7 @@ public class RNSoundModule extends ReactContextBaseJavaModule implements Lifecyc
       e.putString("message", "resource not found");
       return;
     }
+    this.playerPool.put(key, player);
 
     final RNSoundModule module = this;
 
@@ -89,7 +90,6 @@ public class RNSoundModule extends ReactContextBaseJavaModule implements Lifecyc
         if (callbackWasCalled) return;
         callbackWasCalled = true;
 
-        module.playerPool.put(key, mp);
         WritableMap props = Arguments.createMap();
         props.putDouble("duration", mp.getDuration() * .001);
         try {
